68692eb1d49002d25ba566f73a0acaa071992098
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-04 10:42:29 -0300

    feat<brics-economic>: add Country interface

---
+++ brics-economic/src/app/model/brics.ts
@@ -0,0 +1,10 @@
+export interface Country {
+  name: string;
+  population: number;
+  gdp: number;
+  gdpPerCapita: number;
+  hdi: number;
+  debt: number;
+  debtPercentageGDP: number;
+  deficitPercentageGDP: number;
+}
