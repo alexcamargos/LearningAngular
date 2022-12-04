e3c4d7f8608757e51bb0abfa91a93df801e9c54f
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-04 10:45:44 -0300

    feat<brics-economic>: add app modules and routing

---
+++ brics-economic/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MembershipCardComponent } from './membership-card/membership-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MembershipCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
