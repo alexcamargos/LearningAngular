2cf35aa054acb5eba753ff92ac1bbabe4410c710
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-08 13:01:14 -0300

    feat<book-blog>: add component base for featured author

---
+++ book-blog/src/app/components/featured-author/featured-author.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-author',
+  templateUrl: './featured-author.component.html',
+  styleUrls: ['./featured-author.component.sass']
+})
+export class FeaturedAuthorComponent {
+
+}
