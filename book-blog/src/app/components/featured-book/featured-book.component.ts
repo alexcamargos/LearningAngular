eee482ef160b9fe4fe7c8e1b8ea44e4a87972428
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-08 13:01:51 -0300

    feat<book-blog>: add component base for featured book

---
+++ book-blog/src/app/components/featured-book/featured-book.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-book',
+  templateUrl: './featured-book.component.html',
+  styleUrls: ['./featured-book.component.sass']
+})
+export class FeaturedBookComponent {
+
+}
