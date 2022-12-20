eead58c6e532a2edf0b9294886d565a0370873b7
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-20 06:43:18 -0300

    feat<book-blog>: add blog posts data

---
+++ book-blog/src/app/data/books_data.ts
@@ -0,0 +1,210 @@
+export const books = [
+  {
+    id: '1',
+    date: '2020-01-01',
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    rating: 3,
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu scelerisque felis imperdiet proin fermentum leo.
+            Varius quam quisque id diam vel quam elementum pulvinar. Mattis pellentesque id nibh tortor id aliquet lectus proin.
+            Nibh mauris cursus mattis molestie a iaculis at erat. Leo urna molestie at elementum eu facilisis.
+            Eget nunc lobortis mattis aliquam. Quis commodo odio aenean sed adipiscing diam donec.
+            Tristique senectus et netus et malesuada fames. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim.
+            Felis bibendum ut tristique et egestas quis ipsum. Nunc sed velit dignissim sodales ut eu sem.
+            Interdum consectetur libero id faucibus. Viverra nam libero justo laoreet sit amet cursus sit.
+            Cursus metus aliquam eleifend mi in. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.
+            Felis eget velit aliquet sagittis id consectetur purus ut. Eu sem integer vitae justo eget magna.
+            
+            Eu scelerisque felis imperdiet proin fermentum leo vel orci. Adipiscing commodo elit at imperdiet dui accumsan sit amet nulla.
+            Imperdiet dui accumsan sit amet nulla. Diam maecenas sed enim ut sem viverra aliquet eget sit.
+            Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Lectus sit amet est placerat in egestas erat.
+            Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Rhoncus urna neque viverra justo nec ultrices dui sapien.
+            Ac turpis egestas sed tempus urna. Risus commodo viverra maecenas accumsan lacus. Posuere ac ut consequat semper viverra.
+            Libero enim sed faucibus turpis in eu mi bibendum neque. Tortor pretium viverra suspendisse potenti nullam ac.
+            Consequat ac felis donec et odio pellentesque diam. Vulputate sapien nec sagittis aliquam malesuada.
+            Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tincidunt vitae semper quis lectus.`,
+  },
+  {
+    id: '2',
+    date: '2020-01-02',
+    title: 'The Fellowship of the Ring',
+    author: 'J.R.R. Tolkien',
+    rating: 5,
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu scelerisque felis imperdiet proin fermentum leo.
+            Varius quam quisque id diam vel quam elementum pulvinar. Mattis pellentesque id nibh tortor id aliquet lectus proin.
+            Nibh mauris cursus mattis molestie a iaculis at erat. Leo urna molestie at elementum eu facilisis.
+            Eget nunc lobortis mattis aliquam. Quis commodo odio aenean sed adipiscing diam donec.
+            Tristique senectus et netus et malesuada fames. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim.
+            Felis bibendum ut tristique et egestas quis ipsum. Nunc sed velit dignissim sodales ut eu sem.
+            Interdum consectetur libero id faucibus. Viverra nam libero justo laoreet sit amet cursus sit.
+            Cursus metus aliquam eleifend mi in. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.
+            Felis eget velit aliquet sagittis id consectetur purus ut. Eu sem integer vitae justo eget magna.
+
+            Eu scelerisque felis imperdiet proin fermentum leo vel orci. Adipiscing commodo elit at imperdiet dui accumsan sit amet nulla.
+            Imperdiet dui accumsan sit amet nulla. Diam maecenas sed enim ut sem viverra aliquet eget sit.
+            Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Lectus sit amet est placerat in egestas erat.
+            Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Rhoncus urna neque viverra justo nec ultrices dui sapien.
+            Ac turpis egestas sed tempus urna. Risus commodo viverra maecenas accumsan lacus. Posuere ac ut consequat semper viverra.
+            Libero enim sed faucibus turpis in eu mi bibendum neque. Tortor pretium viverra suspendisse potenti nullam ac.
+            Consequat ac felis donec et odio pellentesque diam. Vulputate sapien nec sagittis aliquam malesuada.
+            Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tincidunt vitae semper quis lectus.`,
+  },
+  {
+    id: '3',
+    date: '2020-01-03',
+    title: 'The Two Towers',
+    author: 'J.R.R. Tolkien',
+    rating: 4,
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu scelerisque felis imperdiet proin fermentum leo.
+            Varius quam quisque id diam vel quam elementum pulvinar. Mattis pellentesque id nibh tortor id aliquet lectus proin.
+            Nibh mauris cursus mattis molestie a iaculis at erat. Leo urna molestie at elementum eu facilisis.
+            Eget nunc lobortis mattis aliquam. Quis commodo odio aenean sed adipiscing diam donec.
+            Tristique senectus et netus et malesuada fames. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim.
+            Felis bibendum ut tristique et egestas quis ipsum. Nunc sed velit dignissim sodales ut eu sem.
+            Interdum consectetur libero id faucibus. Viverra nam libero justo laoreet sit amet cursus sit.
+            Cursus metus aliquam eleifend mi in. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.
+            Felis eget velit aliquet sagittis id consectetur purus ut. Eu sem integer vitae justo eget magna.
+
+            Eu scelerisque felis imperdiet proin fermentum leo vel orci. Adipiscing commodo elit at imperdiet dui accumsan sit amet nulla.
+            Imperdiet dui accumsan sit amet nulla. Diam maecenas sed enim ut sem viverra aliquet eget sit.
+            Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Lectus sit amet est placerat in egestas erat.
+            Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Rhoncus urna neque viverra justo nec ultrices dui sapien.
+            Ac turpis egestas sed tempus urna. Risus commodo viverra maecenas accumsan lacus. Posuere ac ut consequat semper viverra.
+            Libero enim sed faucibus turpis in eu mi bibendum neque. Tortor pretium viverra suspendisse potenti nullam ac.
+            Consequat ac felis donec et odio pellentesque diam. Vulputate sapien nec sagittis aliquam malesuada.
+            Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tincidunt vitae semper quis lectus.`,
+  },
+  {
+    id: '4',
+    date: '2020-01-04',
+    title: 'The Return of the King',
+    author: 'J.R.R. Tolkien',
+    rating: 4,
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu scelerisque felis imperdiet proin fermentum leo.
+            Varius quam quisque id diam vel quam elementum pulvinar. Mattis pellentesque id nibh tortor id aliquet lectus proin.
+            Nibh mauris cursus mattis molestie a iaculis at erat. Leo urna molestie at elementum eu facilisis.
+            Eget nunc lobortis mattis aliquam. Quis commodo odio aenean sed adipiscing diam donec.
+            Tristique senectus et netus et malesuada fames. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim.
+            Felis bibendum ut tristique et egestas quis ipsum. Nunc sed velit dignissim sodales ut eu sem.
+            Interdum consectetur libero id faucibus. Viverra nam libero justo laoreet sit amet cursus sit.
+            Cursus metus aliquam eleifend mi in. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.
+            Felis eget velit aliquet sagittis id consectetur purus ut. Eu sem integer vitae justo eget magna.
+
+            Eu scelerisque felis imperdiet proin fermentum leo vel orci. Adipiscing commodo elit at imperdiet dui accumsan sit amet nulla.
+            Imperdiet dui accumsan sit amet nulla. Diam maecenas sed enim ut sem viverra aliquet eget sit.
+            Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Lectus sit amet est placerat in egestas erat.
+            Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Rhoncus urna neque viverra justo nec ultrices dui sapien.
+            Ac turpis egestas sed tempus urna. Risus commodo viverra maecenas accumsan lacus. Posuere ac ut consequat semper viverra.
+            Libero enim sed faucibus turpis in eu mi bibendum neque. Tortor pretium viverra suspendisse potenti nullam ac.
+            Consequat ac felis donec et odio pellentesque diam. Vulputate sapien nec sagittis aliquam malesuada.
+            Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tincidunt vitae semper quis lectus.`,
+  },
+  {
+    id: '5',
+    date: '2020-01-05',
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: 'Douglas Adams',
+    rating: 5,
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu scelerisque felis imperdiet proin fermentum leo.
+            Varius quam quisque id diam vel quam elementum pulvinar. Mattis pellentesque id nibh tortor id aliquet lectus proin.
+            Nibh mauris cursus mattis molestie a iaculis at erat. Leo urna molestie at elementum eu facilisis.
+            Eget nunc lobortis mattis aliquam. Quis commodo odio aenean sed adipiscing diam donec.
+            Tristique senectus et netus et malesuada fames. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim.
+            Felis bibendum ut tristique et egestas quis ipsum. Nunc sed velit dignissim sodales ut eu sem.
+            Interdum consectetur libero id faucibus. Viverra nam libero justo laoreet sit amet cursus sit.
+            Cursus metus aliquam eleifend mi in. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.
+            Felis eget velit aliquet sagittis id consectetur purus ut. Eu sem integer vitae justo eget magna.
+
+            Eu scelerisque felis imperdiet proin fermentum leo vel orci. Adipiscing commodo elit at imperdiet dui accumsan sit amet nulla.
+            Imperdiet dui accumsan sit amet nulla. Diam maecenas sed enim ut sem viverra aliquet eget sit.
+            Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Lectus sit amet est placerat in egestas erat.
+            Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Rhoncus urna neque viverra justo nec ultrices dui sapien.
+            Ac turpis egestas sed tempus urna. Risus commodo viverra maecenas accumsan lacus. Posuere ac ut consequat semper viverra.
+            Libero enim sed faucibus turpis in eu mi bibendum neque. Tortor pretium viverra suspendisse potenti nullam ac.
+            Consequat ac felis donec et odio pellentesque diam. Vulputate sapien nec sagittis aliquam malesuada.
+            Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tincidunt vitae semper quis lectus.`,
+  },
+  {
+    id: '6',
+    date: '2020-01-06',
+    title: 'The Restaurant at the End of the Universe',
+    author: 'Douglas Adams',
+    rating: 4,
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu scelerisque felis imperdiet proin fermentum leo.
+            Varius quam quisque id diam vel quam elementum pulvinar. Mattis pellentesque id nibh tortor id aliquet lectus proin.
+            Nibh mauris cursus mattis molestie a iaculis at erat. Leo urna molestie at elementum eu facilisis.
+            Eget nunc lobortis mattis aliquam. Quis commodo odio aenean sed adipiscing diam donec.
+            Tristique senectus et netus et malesuada fames. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim.
+            Felis bibendum ut tristique et egestas quis ipsum. Nunc sed velit dignissim sodales ut eu sem.
+            Interdum consectetur libero id faucibus. Viverra nam libero justo laoreet sit amet cursus sit.
+            Cursus metus aliquam eleifend mi in. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.
+            Felis eget velit aliquet sagittis id consectetur purus ut. Eu sem integer vitae justo eget magna.
+
+            Eu scelerisque felis imperdiet proin fermentum leo vel orci. Adipiscing commodo elit at imperdiet dui accumsan sit amet nulla.
+            Imperdiet dui accumsan sit amet nulla. Diam maecenas sed enim ut sem viverra aliquet eget sit.
+            Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Lectus sit amet est placerat in egestas erat.
+            Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Rhoncus urna neque viverra justo nec ultrices dui sapien.
+            Ac turpis egestas sed tempus urna. Risus commodo viverra maecenas accumsan lacus. Posuere ac ut consequat semper viverra.
+            Libero enim sed faucibus turpis in eu mi bibendum neque. Tortor pretium viverra suspendisse potenti nullam ac.
+            Consequat ac felis donec et odio pellentesque diam. Vulputate sapien nec sagittis aliquam malesuada.
+            Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tincidunt vitae semper quis lectus.`,
+  },
+  {
+    id: '7',
+    date: '2020-01-07',
+    title: 'Life, the Universe and Everything',
+    author: 'Douglas Adams',
+    rating: 5,
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu scelerisque felis imperdiet proin fermentum leo.
+            Varius quam quisque id diam vel quam elementum pulvinar. Mattis pellentesque id nibh tortor id aliquet lectus proin.
+            Nibh mauris cursus mattis molestie a iaculis at erat. Leo urna molestie at elementum eu facilisis.
+            Eget nunc lobortis mattis aliquam. Quis commodo odio aenean sed adipiscing diam donec.
+            Tristique senectus et netus et malesuada fames. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim.
+            Felis bibendum ut tristique et egestas quis ipsum. Nunc sed velit dignissim sodales ut eu sem.
+            Interdum consectetur libero id faucibus. Viverra nam libero justo laoreet sit amet cursus sit.
+            Cursus metus aliquam eleifend mi in. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.
+            Felis eget velit aliquet sagittis id consectetur purus ut. Eu sem integer vitae justo eget magna.
+
+            Eu scelerisque felis imperdiet proin fermentum leo vel orci. Adipiscing commodo elit at imperdiet dui accumsan sit amet nulla.
+            Imperdiet dui accumsan sit amet nulla. Diam maecenas sed enim ut sem viverra aliquet eget sit.
+            Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Lectus sit amet est placerat in egestas erat.
+            Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Rhoncus urna neque viverra justo nec ultrices dui sapien.
+            Ac turpis egestas sed tempus urna. Risus commodo viverra maecenas accumsan lacus. Posuere ac ut consequat semper viverra.
+            Libero enim sed faucibus turpis in eu mi bibendum neque. Tortor pretium viverra suspendisse potenti nullam ac.
+            Consequat ac felis donec et odio pellentesque diam. Vulputate sapien nec sagittis aliquam malesuada.
+            Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tincidunt vitae semper quis lectus.`,
+  },
+  {
+    id: '8',
+    date: '2020-01-08',
+    title: 'So Long, and Thanks for All the Fish',
+    author: 'Douglas Adams',
+    rating: 5,
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Eu scelerisque felis imperdiet proin fermentum leo.
+            Varius quam quisque id diam vel quam elementum pulvinar. Mattis pellentesque id nibh tortor id aliquet lectus proin.
+            Nibh mauris cursus mattis molestie a iaculis at erat. Leo urna molestie at elementum eu facilisis.
+            Eget nunc lobortis mattis aliquam. Quis commodo odio aenean sed adipiscing diam donec.
+            Tristique senectus et netus et malesuada fames. Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim.
+            Felis bibendum ut tristique et egestas quis ipsum. Nunc sed velit dignissim sodales ut eu sem.
+            Interdum consectetur libero id faucibus. Viverra nam libero justo laoreet sit amet cursus sit.
+            Cursus metus aliquam eleifend mi in. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut.
+            Felis eget velit aliquet sagittis id consectetur purus ut. Eu sem integer vitae justo eget magna.
+
+            Eu scelerisque felis imperdiet proin fermentum leo vel orci. Adipiscing commodo elit at imperdiet dui accumsan sit amet nulla.
+            Imperdiet dui accumsan sit amet nulla. Diam maecenas sed enim ut sem viverra aliquet eget sit.
+            Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Lectus sit amet est placerat in egestas erat.
+            Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Rhoncus urna neque viverra justo nec ultrices dui sapien.
+            Ac turpis egestas sed tempus urna. Risus commodo viverra maecenas accumsan lacus. Posuere ac ut consequat semper viverra.
+            Libero enim sed faucibus turpis in eu mi bibendum neque. Tortor pretium viverra suspendisse potenti nullam ac.
+            Consequat ac felis donec et odio pellentesque diam. Vulputate sapien nec sagittis aliquam malesuada.
+            Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Tincidunt vitae semper quis lectus.`,
+  },
+];
