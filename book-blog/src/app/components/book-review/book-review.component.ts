63a113666ef8740ac55ac14224f92f81114109c7
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-20 06:44:42 -0300

    feat<book-blog>: display the main book review data

--- book-blog/src/app/components/book-review/book-review.component.ts
+++ book-blog/src/app/components/book-review/book-review.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BlogPostService } from '../../services/blog-post.service';
+import { BlogPost } from '../../models/blogPost';
 
 @Component({
   selector: 'app-book-review',
   templateUrl: './book-review.component.html',
-  styleUrls: ['./book-review.component.sass']
+  styleUrls: ['./book-review.component.sass'],
 })
-export class BookReviewComponent {
+export class BookReviewComponent implements OnInit {
+  @Input() blogPosts: BlogPost[];
 
+  constructor(private blogPostService: BlogPostService) {
+    this.blogPosts = this.blogPostService.blogPosts;
+  }
+
+  ngOnInit(): void {
+    console.log(this.blogPosts);
+  }
 }
