34dd4ff36774bb7d6a3149a09d3bcf87aec88dd6
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-24 17:30:46 -0300

    feat<getting-started>: implement TopBarComponent
        The store logo, name and checkout button.

--- getting-started/src/app/components/top-bar/top-bar.component.ts
+++ getting-started/src/app/components/top-bar/top-bar.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.sass']
+  styleUrls: ['./top-bar.component.sass'],
 })
-export class TopBarComponent {
-
-}
+export class TopBarComponent {}
