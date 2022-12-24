80ba39249345f1ca5d442ffe1f145243b9981a14
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-24 12:16:55 -0300

    feat<getting-started>: add products data
        This commit adds the products data to the project.

---
+++ getting-started/src/app/data/products.ts
@@ -0,0 +1,114 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Galago 14"',
+    category: 'laptops',
+    price: 999,
+    description:
+      'Up to 12-Core 12th Gen Intel i7 CPU\nIntel® Iris® Xe Graphics\nUp to 64GB memory\nUp to 2TB NVMe storage\n3.2 lbs (1.45 kg) base weight',
+  },
+  {
+    id: 2,
+    name: 'Lemur Pro 14"',
+    category: 'laptops',
+    price: 1199,
+    description:
+      'Up to 10-Core 12th Gen Intel Core i7\nIntel® Iris® Xe Graphics\nUp to 40GB memory\nUp to 4TB NVMe storage\n2.54 lbs (1.15kg) base weight',
+  },
+  {
+    id: 3,
+    name: 'Darter Pro 15"',
+    category: 'laptops',
+    price: 1059,
+    description:
+      'Up to 12-Core 12th Gen Intel i7 CPU\nIntel® Iris® Xe Graphics\nUp to 64GB memory\nUp to 4TB NVMe storage\n3.84 lbs (1.7 kg) base weight',
+  },
+  {
+    id: 4,
+    name: 'Gazelle 15" or 17"',
+    category: 'laptops',
+    price: 1299,
+    description:
+      '12th Gen Intel i7 CPU\nNVIDIA GeForce RTX Graphics\nUp to 64GB memory\nUp to 4TB NVMe storage\n4.39 lbs (1.99 kg) base weight',
+  },
+  {
+    id: 5,
+    name: 'Oryx Pro 15" or 17"',
+    category: 'laptops',
+    price: 2199,
+    description:
+      '12th Gen Intel i7 CPU\nNVIDIA GeForce RTX Graphics\nUp to 64GB DDR5 memory\nUp to 4TB NVMe storage\n5.29 lbs (2.4 kg) base weight',
+  },
+  {
+    id: 6,
+    name: 'THELIO',
+    category: 'desktops',
+    price: 1149,
+    description:
+      'Intel 13th Gen or AMD Ryzen™ 7000 CPU\nUp to 64GB of Memory\nUp to GeForce 3070 GPUs\nUp to 56TB of Storage\nSpace-Saving Design',
+  },
+  {
+    id: 7,
+    name: 'THELIO MIRA',
+    category: 'desktops',
+    price: 1199,
+    description:
+      'Intel 13th Gen or AMD Ryzen™ 7000 CPU\nUp to 128GB of Memory\nUp to GeForce 3080Ti GPUs\nUp to 56TB of Storage\nThe Professional Desktop',
+  },
+  {
+    id: 8,
+    name: 'THELIO MAJOR',
+    category: 'desktops',
+    price: 2499,
+    description:
+      'AMD Threadripper 3, Ryzen™ 7000, or Intel 13th Gen CPUs\nUp to 256GB of Memory\nUp to NVIDIA 4090 GPUs\nUp to 88 TB of Storage\nMarathon Performance',
+  },
+  {
+    id: 9,
+    name: 'THELIO MEGA',
+    category: 'desktops',
+    price: 4699,
+    description:
+      'Threadripper PRO 5000 WX-Series CPUs\nUp to 256GB of ECC Memory\nUp to 4 GPUs\nUp to 52TB of Storage\nAccelerated Compute Proficiency',
+  },
+  {
+    id: 10,
+    name: 'THELIO MASSIVE',
+    category: 'desktops',
+    price: 3699,
+    description:
+      'Dual XEON CPUs\nUp to 1536GB of ECC Memory\nUp to 4 GPUs\nUp to 72TB of Storage\nDependable, Continuous Computing',
+  },
+  {
+    id: 11,
+    name: 'Meerkat',
+    category: 'mini-pcs',
+    price: 499,
+    description:
+      'Up to 16 TB of storage in a 4.5” square.\nWe love space in any form.\n10th or 11th Gen Intel Core i Series\nUp to 16 TB of storage\nMere 4.5″ square of desk space\nEnergy efficient',
+  },
+  {
+    id: 12,
+    name: 'Eland 1U',
+    category: 'servers',
+    price: 3399,
+    description:
+      'Single Socket AMD® EPYC® processors\n4x 3.5” or 10x 2.5” NVMe hot swap drive bays\nUp to 8 Channel 16x 3200 MHz ECC memory\nDefault Redundant power supply',
+  },
+  {
+    id: 13,
+    name: 'Eland Pro 1U',
+    category: 'servers',
+    price: 3999,
+    description:
+      'Dual AMD® EPYC® processors\n10x 2.5” hot swap drive bays\nUp to 8 Channel 16x 3200 MHz ECC memory per processor\nDefault Redundant power supply',
+  },
+  {
+    id: 14,
+    name: 'Eland Pro 2U',
+    category: 'servers',
+    price: 4399,
+    description:
+      'Dual AMD® EPYC® processors\n12x 3.5” or 24x 2.5” hot swap drive bays\nUp to 8 Channel 16x 3200 MHz ECC memory per processor\nDefault Redundant power supply',
+  },
+];
