9f78be66aa53a4d3315d3b547cbc6cd2c54ac871
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-24 17:52:59 -0300

    feat<getting-started>: implement product list component

--- getting-started/src/app/components/product-list/product-list.component.ts
+++ getting-started/src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product';
+import { products } from '../../data/products';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.sass']
+  styleUrls: ['./product-list.component.sass'],
 })
 export class ProductListComponent {
+  products: Product[] = products;
 
+  constructor() {
+    this.products = products;
+  }
 }
