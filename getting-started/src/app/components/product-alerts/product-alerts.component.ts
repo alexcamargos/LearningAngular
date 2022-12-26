ffd61990ba4c1248c81ff1eece1d375d27156d02
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-26 06:52:42 -0300

    feat<getting-started>: add product alerts for target price
        If the price of a product is between lowTargetPrice
        and highTargetPrice, the product will be highlighted for sharing.
    
            lowTargetPrice = 1000
            highTargetPrice = 2000

--- getting-started/src/app/components/product-alerts/product-alerts.component.ts
+++ getting-started/src/app/components/product-alerts/product-alerts.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.sass']
+  styleUrls: ['./product-alerts.component.sass'],
 })
 export class ProductAlertsComponent {
+  @Input() product!: Product;
 
+  lowTargetPrice = 1000;
+  highTargetPrice = 2000;
+
+  constructor() {}
+
+  share(product: Product) {
+    window.alert(`The ${product.name} has been shared!`);
+  }
 }
