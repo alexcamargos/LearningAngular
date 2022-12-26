9a42b38f46f83f9573f958b2b15690f17a7085f8
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-26 20:53:49 -0300

    feat<getting-started>: import '@angular/material/card'

--- getting-started/src/app/angular-material.module.ts
+++ getting-started/src/app/angular-material.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  exports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  exports: [MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule],
 })
 export class AngularMaterialModule {}
