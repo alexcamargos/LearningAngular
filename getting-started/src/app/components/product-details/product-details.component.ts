1f72d2bc7ea0c648f70bb1016640b7746db2657d
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-26 21:38:23 -0300

    feat<getting-started>: implement product details component

--- getting-started/src/app/components/product-details/product-details.component.ts
+++ getting-started/src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../../data/products';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.sass']
+  styleUrls: ['./product-details.component.sass'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    // Get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('id'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
 }
