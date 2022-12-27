239b9bafa881224f3d1f09114c2aa3cab7d486f5
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-26 21:35:50 -0300

    feat: add new routing
        path: 'products/:id', component: ProductDetailsComponent

--- getting-started/src/app/app-routing.module.ts
+++ getting-started/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'products', component: HomeComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
 ];
 
 @NgModule({
