28b8b517d326bded692e12f2e9dbefd2b531b871
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-28 20:31:01 -0300

    feat<getting-started>: add component for manage cart
        This component will be responsible for managing the cart.

--- getting-started/src/app/components/cart/cart.component.ts
+++ getting-started/src/app/components/cart/cart.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CartService } from 'src/app/services/cart.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.sass']
+  styleUrls: ['./cart.component.sass'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  items: Product[] = [];
 
+  constructor(private cart: CartService) {}
+
+  ngOnInit() {
+    this.items = this.cart.getItems();
+  }
+
+  removeItem(item: Product) {
+    this.items = this.cart.removeItem(item);
+  }
 }
