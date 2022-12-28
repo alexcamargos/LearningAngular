8099fe261fe6aace496bad65a2817b1ce48348cb
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-12-28 19:43:29 -0300

    feat: add /cart routing
        This routing will be used to show the cart page.

--- getting-started/src/app/app-routing.module.ts
+++ getting-started/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'products', component: HomeComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
